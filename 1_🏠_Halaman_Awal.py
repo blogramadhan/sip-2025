c05fb9590fa97d02c3bd8ccdc880759c89b2a89c
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2025-03-11 09:39:50 +0700

    update code

--- 1_🏠_Halaman_Awal.py
+++ 1_🏠_Halaman_Awal.py
@@ -26,6 +26,16 @@ st.set_page_config(
 # Membuat Logo
 logo()
 
+menu_sidebar = {
+    "Your account": [
+        st.Page("create_account.py", title="Create your account"),
+        st.Page("manage_account.py", title="Manage your account"),
+    ],
+    "Resources": [
+        st.Page("learn.py", title="Learn about us"),
+        st.Page("trial.py", title="Try it out"),
+    ],
+}
 st.navigation(menu_sidebar)
 
 # Membuat Judul Halaman
