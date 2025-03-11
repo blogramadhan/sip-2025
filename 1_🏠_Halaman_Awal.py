784f69e2d69bc9628c8f5d7f406a9716c47eb200
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2025-03-11 09:40:48 +0700

    update code

--- 1_🏠_Halaman_Awal.py
+++ 1_🏠_Halaman_Awal.py
@@ -28,12 +28,8 @@ logo()
 
 menu_sidebar = {
     "Your account": [
-        st.Page("create_account.py", title="Create your account"),
-        st.Page("manage_account.py", title="Manage your account"),
     ],
     "Resources": [
-        st.Page("learn.py", title="Learn about us"),
-        st.Page("trial.py", title="Try it out"),
     ],
 }
 st.navigation(menu_sidebar)
