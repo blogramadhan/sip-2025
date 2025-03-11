67cda6dae9963c96a52d9f3d754d9d7bff94f152
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2025-03-11 13:05:36 +0700

    update code

--- pages/2_📋_SIRUP.py
+++ pages/2_📋_SIRUP.py
@@ -24,7 +24,7 @@ logo()
 
 # Membuat UKPBJ
 daerah = region_config()
-pilih = st.sidebar.selectbox("Pilih Daerah", list(config.keys()))
+pilih = st.sidebar.selectbox("Pilih Daerah", list(daerah.keys()))
 tahun = st.sidebar.selectbox("Pilih Tahun", range(datetime.now().year, datetime.now().year-3, -1))
 selected_daerah = daerah.get(pilih, {})
 kodeFolder = selected_daerah.get("folder")
