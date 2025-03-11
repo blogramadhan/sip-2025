8f40707d49d3f2cf2fa3ccd51cf1f507820a5805
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2025-03-11 14:19:14 +0700

    update code

--- pages/2_📋_SIRUP.py
+++ pages/2_📋_SIRUP.py
@@ -119,26 +119,38 @@ with menu_rup_5:
             column_config={
                 "STRUKTUR_ANGGARAN": st.column_config.NumberColumn(
                     "STRUKTUR ANGGARAN",
-                    format="Rp %,.2f"
+                    help="Nilai Struktur Anggaran",
+                    min_value=0,
+                    format=lambda x: f"Rp {x:,.2f}"
                 ),
                 "RUP_PENYEDIA": st.column_config.NumberColumn(
                     "RUP PAKET PENYEDIA",
-                    format="Rp %,.2f"
+                    help="Nilai RUP Paket Penyedia",
+                    min_value=0,
+                    format=lambda x: f"Rp {x:,.2f}"
                 ),
                 "RUP_SWAKELOLA": st.column_config.NumberColumn(
-                    "RUP PAKET SWAKELOLA", 
-                    format="Rp %,.2f"
+                    "RUP PAKET SWAKELOLA",
+                    help="Nilai RUP Paket Swakelola",
+                    min_value=0,
+                    format=lambda x: f"Rp {x:,.2f}"
                 ),
                 "TOTAL_RUP": st.column_config.NumberColumn(
                     "TOTAL RUP",
-                    format="Rp %,.2f"
+                    help="Total Nilai RUP",
+                    min_value=0,
+                    format=lambda x: f"Rp {x:,.2f}"
                 ),
                 "SELISIH": st.column_config.NumberColumn(
                     "SELISIH",
-                    format="Rp %,.2f"
+                    help="Selisih Nilai",
+                    min_value=0,
+                    format=lambda x: f"Rp {x:,.2f}"
                 ),
                 "PERSEN": st.column_config.NumberColumn(
                     "PERSENTASE",
+                    help="Persentase Input RUP",
+                    min_value=0,
                     format="%.2f%%"
                 )
             },
