278a7a78835159ec0cbf7d0b960c802053700bd1
Kurnia Ramadhan <kramadhan@gmail.com>
2025-05-08 13:44:14 +0700

    Memperbarui impor di file ekatalog.py dengan menambahkan titik koma di akhir baris untuk meningkatkan konsistensi sintaks. Perubahan ini bertujuan untuk memperbaiki potensi masalah dalam eksekusi kode.

--- code/ekatalog.py
+++ code/ekatalog.py
@@ -8,7 +8,7 @@ import duckdb
 from datetime import datetime
 from st_aggrid import AgGrid, GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
-from streamlit_extras.metric_cards import style_metric_cards
+from streamlit_extras.metric_cards import style_metric_cards;
 from fungsi import *
 
 # Konfigurasi UKPBJ
