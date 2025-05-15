1725eee5ded15f197f96e6a4ae5fc3afb478b686
Kurnia Ramadhan <kramadhan@gmail.com>
2025-05-15 13:29:37 +0700

    Remove unused imports in pencatatan.py to streamline the code and improve readability. This includes the removal of numpy, add_logo, and SocialMediaIcons, which were not utilized in the current implementation.

--- code/pencatatan.py
+++ code/pencatatan.py
@@ -1,7 +1,6 @@
 # Library Utama
 import streamlit as st
 import pandas as pd
-import numpy as np
 import plotly.express as px
 import duckdb
 import openpyxl
@@ -9,13 +8,10 @@ from datetime import datetime
 # Library Currency
 from babel.numbers import format_currency
 # Library Aggrid
-from st_aggrid import AgGrid, GridUpdateMode
+from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 # Library Streamlit-Extras
 from streamlit_extras.metric_cards import style_metric_cards
-from streamlit_extras.app_logo import add_logo
-# Library Social Media Links
-from st_social_media_links import SocialMediaIcons
 # Library Tambahan
 from fungsi import *
 
