0730bd610e700ae6c56addee69233bb33c4703af
Kurnia Ramadhan <kramadhan@gmail.com>
2025-05-19 12:18:24 +0700

    Update data retrieval in tender.py to use drop_duplicates() for the 'TenderBAST' dataset, ensuring unique entries and enhancing data integrity for subsequent metric calculations.

--- code/tender.py
+++ code/tender.py
@@ -660,7 +660,7 @@ with menu_tender_4:
 with menu_tender_5:
     try:
         # Baca dataset BAPBAST
-        dfSPSETenderBAST = read_df_duckdb(datasets["TenderBAST"]).unique()
+        dfSPSETenderBAST = read_df_duckdb(datasets["TenderBAST"]).drop_duplicates()
 
         # Header dan tombol unduh
         col1, col2 = st.columns([7,3])
